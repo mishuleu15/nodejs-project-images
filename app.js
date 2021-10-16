2c809fa24b1aa25a6244d05fa55ba62fe8e1cf88
unknown <mishu_leu15@yahoo.com>
2021-10-16 16:33:14 +0300

    added another route for login and created the login page with styling

--- app.js
+++ app.js
@@ -3,26 +3,40 @@ const fs = require('fs');
 
 const app = express();
 
-const PORT = 8000;
+app.use('/img', express.static('./img'));
 
-const replaceTemplate = (temp, element) => {
-  let output = temp.replace(/{%TITLE%}/g, element.title);
-  return output;
-};
+const PORT = 8000;
 
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   'utf-8'
 );
 
+const loginForm = fs.readFileSync(
+  `${__dirname}/templates/login-form.html`,
+  'utf-8'
+);
+
 const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
+
+function replaceTemplate(temp, element) {
+  const { title, login, submit } = element;
+  let output = temp.replace(/{%TITLE%}/g, title);
+  output = output.replace(/{%LOGIN%}/g, login);
+  output = output.replace(/{%SUBMIT%}/g, submit);
+  return output;
+}
 
 const dataObj = JSON.parse(data);
 
 app.get('/', (req, res) => {
+  // console.log(req.params.id);
   const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
-  const output = tempCard.replace('{%TITLE%}', cardsHtml);
-  res.status(200).send(output);
+  res.status(200).send(cardsHtml);
+});
+
+app.get('/login', (req, res) => {
+  res.status(200).send(loginForm);
 });
 
 app.listen(PORT, () => {
